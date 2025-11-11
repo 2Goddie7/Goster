a44050769755c34e0e23267af14428e99ff6b630
2Goddie7 <diego.mullo@epn.edu.ec>
2025-11-10 22:41:57 -0500

    clear

--- src/core/config/firebase.ts
+++ src/core/config/firebase.ts
@@ -3,13 +3,7 @@ import { getAuth } from 'firebase/auth';
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCsA1gpPy3wkCQWXfiqtdOPrmcUZns_Gqc",
-  authDomain: "goster-1b588.firebaseapp.com",
-  projectId: "goster-1b588",
-  storageBucket: "goster-1b588.firebasestorage.app",
-  messagingSenderId: "28848336169",
-  appId: "1:28848336169:web:727f995395a9b7f3b15b38",
-  measurementId: "G-3TP5701TK6"
+  
 };
 
 // Inicializar Firebase
